a11a7752b6527ba09b6311e530b9e55839f59565
Mayron Alves <mayronalves@alu.ufc.br>
2021-09-17 14:52:09 -0300

    Adicionado metodo adicioanr e listar do array

--- app/controllers/negociacao-controler.ts
+++ app/controllers/negociacao-controler.ts
@@ -1,9 +1,11 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 
 export class NegociacaoControler {
   private inputData: HTMLInputElement;
   private inputQuantidade: HTMLInputElement;
   private inputValor: HTMLInputElement;
+  private negociacoes : Negociacoes = new Negociacoes();
 
   constructor() {
     this.inputData = document.querySelector("#data");
@@ -13,7 +15,8 @@ export class NegociacaoControler {
 
   adicionar() : void {
     const negociacao : Negociacao = this.criaNegociacao();
-    console.log(negociacao);
+    this.negociacoes.adiciona(negociacao);
+    console.log(this.negociacoes.lista());
     this.limparFormulario();
   }
 
